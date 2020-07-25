bc4c883848b6f76727e2c7835a2abb391354163d
rahulraj1541996 <60652862+rahulraj1541996@users.noreply.github.com>
2020-07-25 12:44:10 +0530

    Add files via upload

---
+++ Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItem
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int menuItemId { get; set; }
+        public string menuItemName { get; set; }
+    }
+}
